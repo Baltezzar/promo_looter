8bb02645460084df8653a0c79c6c8ce0cf965244
baltezar <baltezar9999@gmail.com>
2026-08-17 01:58:25 +0300

    add sent_at timestamp

--- baltezar.py
+++ baltezar.py
@@ -5,8 +5,9 @@ import pyperclip
 import keyboard
 import threading
 import time
+import pyautogui
 
-SERVER_URL = "https://мой-адрес.onrender.com"
+SERVER_URL = "https://promo-looter.onrender.com"
 char_count = 1
 buffer = [] # накапливаем символы пока не наберём нужное кол-во
 # Флаги
@@ -20,24 +21,44 @@ def fetch_chars():
         data = response.json()
         chars = data["chars"]
         if len(chars) > 0:
-            buffer.extend(chars) # extend в отличии от append добавляет не один элемент, а сразу список 
+            item = chars[0] # первый элемент это словарь
+            buffer.extend([item["char"]]) # extend в отличии от append добавляет не один элемент, а сразу список 
+            sent_at = item["sent_at"]
             if len(buffer) >= char_count:
                 result = "".join(buffer[:char_count]) # берём только нужное кол-во символов
                                                       # символ ":" - означает от начала и до char_count, а join() склеивает символы типа 'a', 'b' в одну строку - 'ab' потому что "" стоит пере join, "".join()... если поставить так: "-".join(['a', 'b']) -> a-b 
                 buffer.clear()
                 pyperclip.copy(result) # кладем в буфер обмена
                 is_pasted = False # сбрасываем флаг - новый символ пришёл, можно вставлять
-                print(f"В буфере: {result}")
+                delay = time.time() - sent_at
+                print(f"В буфере: {result} | Задержка до буфера: {delay*1000:.0f}мс")
     except Exception as e:
         print(f"Ошибка получения: {e}")      
 
 
+# def on_ctrl_v():
+#     global is_pasted
+#     if is_pasted:
+#         return False # Блокируем повторное нажатие
+#     if pyperclip.paste() != "":
+#         is_pasted = True
+#         return True
+#     return False 
+
+
 def on_ctrl_v():
     global is_pasted
+
     if is_pasted:
-        return False # Блокируем повторное нажатие
-    if pyperclip.paste() != "":
-        is_pasted = True
+        return
+
+    if pyperclip.paste() == "":
+        return
+
+    is_pasted = True
+    paste_delay = time.time() - sent_at # нужно sent_at сделать глобальной
+    print(f"Задержка до вставки: {paste_delay*1000:.0f}мс")
+    pyautogui.hotkey("ctrl", "v")
 
 
 # Функция нажатия на f1 - очистка буфера обмена
@@ -56,5 +77,12 @@ def start_polling():
 
 threading.Thread(target=start_polling, daemon=True).start()
 keyboard.add_hotkey('f1', on_f1)
-keyboard.add_hotkey('ctrl+v', on_ctrl_v, suppress=True) # suppress=True - перехватываем ctrl+v и решаем сами пускать его или нет  
+# keyboard.add_hotkey('ctrl+v', on_ctrl_v, suppress=True) # suppress=True - перехватываем ctrl+v и решаем сами пускать его или нет  
+
+keyboard.add_hotkey(
+    "ctrl+v",
+    on_ctrl_v,
+    suppress=True
+)
+
 keyboard.wait()               
--- kirill.py
+++ kirill.py
@@ -3,7 +3,7 @@ import keyboard
 import threading
 import time
 
-SERVER_URL = "https://мой-адрес.onrender.com"
+SERVER_URL = "https://promo-looter.onrender.com"
 char_count = 1 # кол-во символов которые будут обрабатываться для копирование в буфер обмена и отправка на сервер. Задается на моем сервере через cmd
 
 is_active = False # флаг чтобы включать и выключать скан символов без выключения скрипта
@@ -11,10 +11,13 @@ is_active = False # флаг чтобы включать и выключать �
 # Метод отправки символа на сервер
 def send_char(char):
     try:
-        requests.post(f"{SERVER_URL}/send", json={"char": char})
+        requests.post(f"{SERVER_URL}/send", json={
+            "char": char,
+            "sent_at": time.time() # время отправки в секундах
+        })
         print(f"Отправлен символ: {char}")
     except Exception as e:
-        print(f"Ошиька отправки: {e}")
+        print(f"Ошибка отправки: {e}")
 
 # по нажатию на f1 включается и выключается скан
 def on_f1():
@@ -36,13 +39,15 @@ def ping_server():
 
 # Нажали на кнопку, сюда попадает событие
 def on_key(event): 
+    if event.name == 'f1':
+        on_f1()
+        return
     if not is_active: # если скрипт выключен - игнорируем нажатия
         return
     if len(event.name) == 1: # фильтруем служебные клавиши типы shift, ctrl, f1 - у них name длиннее одного символа
         send_char(event.name) # и отправляем нажатую клавишу в метод для отправки на сервер
 
 
-keyboard.add_hotkey('f1', on_f1)
 threading.Thread(target=ping_server, daemon=True).start()
 keyboard.on_press(on_key) # keyboard - слушает клавишы глобально, даже когда окно не в фокусе.
 keyboard.wait() # держит скрипт живым бесконечно                   
